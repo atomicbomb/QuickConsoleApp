7f111ea2df4c89eadd07e51f980d50772aa4b490
Chris Coppin <chris@luminary.com>
2023-03-27 12:27:24 +1100

    Added project files.

---
+++ Helper.cs
@@ -0,0 +1,99 @@
+﻿using CliWrap;
+using CliWrap.Buffered;
+
+namespace QuickConsoleApp;
+
+internal static class Helper
+{
+    internal static string GetProjectName()
+    {
+        Console.Clear();
+        Console.Write("Please enter the project name: ");
+        var projectName = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(projectName))
+        {
+            Console.WriteLine("Exiting the Application....");
+            Thread.Sleep(1500);
+            Environment.Exit(0);
+        }
+
+        return projectName;
+    }
+
+    internal static bool IsPersonalProject()
+    {
+        Console.Clear();
+        Console.Write("Is this a personal project? (y/n): ");
+        var personalProject = Console.ReadKey();
+
+        if (personalProject.KeyChar == 'y')
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    internal static async Task GenerateConsoleProject(string workingDirectory, string projectName)
+    {
+        Console.Clear();
+        Console.WriteLine("Generating console application project, please wait...");
+
+        var results = await Cli.Wrap("dotnet")
+            .WithArguments(new[] { "new", "console", "-o", $"{projectName}" })
+            .WithWorkingDirectory(workingDirectory)
+            .ExecuteBufferedAsync();
+
+            if (!string.IsNullOrEmpty(results.StandardError))
+            {
+                Console.WriteLine($"Error creating console application project: {results.StandardError}");
+                Console.WriteLine("Exiting the Application....");
+                Thread.Sleep(1500);
+                Environment.Exit(0);
+            }
+            else
+            {
+                var createdIn = (results.ExitTime - results.StartTime).TotalSeconds;
+                Console.WriteLine($"Project '{projectName}' successfully created in {createdIn} seconds");
+            }
+
+        Console.WriteLine("");
+        Console.WriteLine("Restoring project dependencies, please wait...");
+
+        results = await Cli.Wrap(@"dotnet")
+            .WithArguments(new[] { "restore" })
+            .WithWorkingDirectory(@$"{workingDirectory}\{projectName}")
+            .ExecuteBufferedAsync();
+
+        Console.Clear();
+
+        if (!string.IsNullOrEmpty(results.StandardError))
+        {
+            Console.WriteLine($"Error resolving project dependencies: {results.StandardError}");
+            Console.WriteLine("Exiting the Application....");
+            Thread.Sleep(1500);
+            Environment.Exit(0);
+        }
+        else
+        {
+            Console.WriteLine($"Project dependencies successfully restored. Opening VSCode...");
+        }
+    }
+
+    internal static async Task OpenConsoleProject(string workingDirectory, string projectName)
+    {
+        var results = await Cli.Wrap(@"code")
+            .WithArguments(new[] { "." })
+            .WithWorkingDirectory(@$"{workingDirectory}\{projectName}")
+            .ExecuteBufferedAsync();
+
+        Console.Clear();
+
+        if (!string.IsNullOrEmpty(results.StandardError)) {
+            Console.WriteLine($"Error opening VSCode: {results.StandardError}");
+        } else {
+            Console.WriteLine("Console Application successfully created.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using QuickConsoleApp;
+using Microsoft.Extensions.Configuration;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        #region Configure appSettings.json
+        var builder = new ConfigurationBuilder();
+        builder.SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+        IConfiguration config = builder.Build();
+        #endregion
+
+        var workingDirectory = config["WorkingDirectory"];
+        var personalProjectsDirectory = config["PersonalProjectsDirectory"];
+
+        var projectName = Helper.GetProjectName();
+        var isPersonalProject = Helper.IsPersonalProject();
+
+        if (isPersonalProject) {
+            workingDirectory += personalProjectsDirectory;
+        }
+
+        await Helper.GenerateConsoleProject(workingDirectory, projectName);
+
+        await Helper.OpenConsoleProject(workingDirectory, projectName);
+
+        Console.WriteLine("Exiting...");
+        Thread.Sleep(1500);
+        Environment.Exit(0);
+    }
+}
